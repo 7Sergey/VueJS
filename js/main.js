9131840186c8f89affcc67f4686ad4be539beb01
Sergey Fedorov <sergey14fedorov@gmail.com>
2019-01-18 21:08:25 +0300

    component

--- js/main.js
+++ js/main.js
@@ -1,33 +1,24 @@
-Vue.filter ('capatilize', function(value){
-    if(!value) return ''
-    value = value.toString()
-    return value.replace(/\b\w/g, function(l){return l.toUpperCase() })
+Vue.component ('app-car', {
+    data: function(){
+        return {
+            cars: [
+                {model:"toyota"},
+                {model:"bmw"},
+                {model:"opel"},
+                {model:"mazda"},
+                {model:"nissan"},
+                {model:"dodge"}
+            ]
+        }
+    },
+    template:'<div><div  class="car" v-for="car in cars"><p>{{car.model}}</p></div></div>'
 });
 
-new Vue ({
-    el:'#app',
-    data: {
-        show: false,
-        message: 'Hello, mister president',
-        cars: [
-            {model:"Турик вешка 2.5", speed:"300"},
-            {model:"nissan", speed:"250"},
-            {model:"audi", speed:"230"},
-            {model:"bmw",speed:"170"}
-        ],    
-    },
-    computed: {
-        showMess(){
-            return this.message.toUpperCase();
-        } 
-    },
-    filters:{
-        lowercase(value) {
-            return value.toLowerCase();
-        },
-        uppercase(value) {
-            return value.toUpperCase();
-        }   
-    }
+new Vue({
+    el:'#app'
+});
+
+new Vue({
+    el:'#app2'
 });
 
